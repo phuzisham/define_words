f9c0082efb8e6de275911849b4ea5d99bd36b2d2
David Hale <david.hale7@gmail.com>
2017-09-15 13:14:42 -0700

    create and display sample words on load

--- app.rb
+++ app.rb
@@ -5,6 +5,19 @@ require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
+  sample_word1 = Word.new({:name=>'Syzygy', :definition=>'A conjunction or opposition, especially of the moon with the sun.'})
+  sample_word2 = Word.new({:name=>'Absquatulate', :definition=>'To leave somewhere abruptly.'})
+  sample_word3 = Word.new({:name=>'Petrichor', :definition=>'That cool smell outside after it rains. Yep...you knew there was a word for it!'})
+  sample_word4 = Word.new({:name=>'Quasar', :definition=>'A massive object in space that emits a tremendous amount of energy.'})
+  sample_word1.save
+  sample_word2.save
+  sample_word3.save
+  sample_word4.save
+  @words = Word.sort
+  erb(:input)
+end
+
+get('/back') do
   @words = Word.sort
   erb(:input)
 end
